88e631408bcf340bac95e71206e7e7f783754aef
Ethan Biggs <ethanscott402@gmail.com>
2025-03-05 09:19:21 -0700

    initial formatting

--- Mission09Biggs/src/App.tsx
+++ Mission09Biggs/src/App.tsx
@@ -1,13 +1,21 @@
-import './App.css'
+import "./App.css";
+
+function TeamList() {
+  return (
+    <>
+      <h3>Test</h3>
+    </>
+  );
+}
 
 function App() {
-
   return (
     <>
       <h1>Welcome to March Madness</h1>
       <h5>Choose your team:</h5>
+      <TeamList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
